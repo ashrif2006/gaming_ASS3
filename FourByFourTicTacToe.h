c0552b63673a906824251c79f0c4c9ce1e24c45e
M0Saber <eng.mo.saber11@gmial.com>
2025-11-24 15:48:21 +0200

    Updating 4 By 4 Tic-Tac-Toe

--- FourByFourTicTacToe.h
+++ FourByFourTicTacToe.h
@@ -2,20 +2,9 @@
 #define FOURBYFOUR_TICTACTOE_H
 
 #include "BoardGame_Classes.h"
-#include <vector>
-#include <string>
+#include <utility>
 
 class FourByFourBoard : public Board<char> {
-private:
-    struct Piece {
-        int x, y;
-        char symbol;
-        Piece(int x, int y, char s) : x(x), y(y), symbol(s) {}
-    };
-    
-    std::vector<Piece> pieces;  // جميع القطع على اللوحة
-    char current_player_symbol;
-
 public:
     FourByFourBoard();
     bool update_board(Move<char>* move) override;
@@ -23,14 +12,11 @@ public:
     bool is_lose(Player<char>* player) override;
     bool is_draw(Player<char>* player) override;
     bool game_is_over(Player<char>* player) override;
-    
-    // دالة مساعدة للتحقق من الحركة القانونية
-    bool is_valid_move(int from_x, int from_y, int to_x, int to_y, char symbol);
-    
-    // دالة للعثور على قطعة في موقع معين
-    int find_piece_index(int x, int y);
-    
-    // دالة لتحريك قطعة
+
+    // دالة مساعدة للتحقق من الحركة
+    bool is_valid_move(int from_x, int from_y, int to_x, int to_y, char symbol) const;
+
+    // دالة لتحريك القطعة مباشرة
     bool move_piece(int from_x, int from_y, int to_x, int to_y, char symbol);
 };
 
@@ -39,9 +25,6 @@ public:
     FourByFourUI();
     Move<char>* get_move(Player<char>* player) override;
     Player<char>* create_player(std::string& name, char symbol, PlayerType type) override;
-    
-    // دالة مساعدة للحصول على حركة معقدة (من-إلى)
-    Move<char>* get_complex_move(Player<char>* player);
 };
 
 #endif
