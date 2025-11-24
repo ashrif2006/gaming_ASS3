2c2d0620a4b54e0ed2d6d8cc61c10cccee9f96c8
Sara Hany Mostafa <sara6hany@gmail.com>
2025-11-24 16:56:43 +0200

    #

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "sus.h"                 //1
 #include "InfinityTicTacToe.h"   // 11
 #include "FourInARow.h"          // 2
+#include "FiveByFiveTicTacToe.h" // 3
 
 using namespace std;
 
@@ -20,6 +21,7 @@ void menu()
         cout << "\t3- 4x4 Tic-Tac-Toe\n";
         cout << "\t4- Pyramid Tic-Tac-Toe\n";
         cout << "\t5- S U S\n";
+		cout << "\t6- 5x5 Tic-Tac-Toe\n";
 		cout << "\t11- Infinity Tic-Tac-Toe\n";
         cout << "\t12- Four In A Row\n";
         cout << "\t6- Goodby\n";
@@ -100,6 +102,19 @@ void menu()
             delete[] players;
             break;
 		}
+        case 6:
+        {
+            FiveByFiveBoard* board = new FiveByFiveBoard();
+            FiveByFiveUI* ui = new FiveByFiveUI();
+            Player<char>** players = ui->setup_players();
+            GameManager<char> gm(board, players, ui);
+            gm.run();
+            delete board;
+            delete ui;
+            delete[] players;
+            break;
+		}
+
         case 11:
         {
             InfinityTicTacToe *board = new InfinityTicTacToe();
@@ -125,7 +140,7 @@ void menu()
             break;
 		}
 
-        case 6:
+        case 7:
         {
             cout << "Goodbye!\n";
             return;
